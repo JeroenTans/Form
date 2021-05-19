9e247146b705caf1dcb4a80b39628c0eca41f316
JeroenTans <jeroentans1@gmail.com>
2021-05-19 14:33:09 +0200

    3.7React af

--- src/App.js
+++ src/App.js
@@ -1,72 +1,79 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
     const [nameValue, setNameValue] = React.useState("")
     const [ageValue, setAgeValue] = React.useState("")
-    const [messageValue, setMessageValue] = React.useState("")
-    const [allInfoValue, setAllInfoValue] = React.useState("")
-  return (
-      <form
-          id="completeForm"
-          onSubmitCapture={allInfoValue}
-          onSubmit={(e)=> setAllInfoValue(e.target)}>
-    <div className= "allBoxes">
-        <div className="boxOne">
-        <form className= "information">
-            <fieldset className="fieldSetOne">
-                <legend>Gegevens</legend>
-                <label
-                    htmlFor="name">Naam:
-                    <input  type="text"
-                            id="inputName"
-                            value={nameValue}
-                            onChange={(e)=> setNameValue(e.target.value)}
-                    />
-                    </label> <br/><br/>
-                <label htmlFor="age">Leeftijd:
-                    <input  type="text"
-                            id="inputAge"
-                            value={ageValue}
-                            onChange={(e)=> setAgeValue(e.target.value)}
-                    /></label>
-            </fieldset>
-        </form><br/>
-        </div>
-        <div className="boxTwo">
-        <form className="review">
-            <fieldset className= "fieldSetTwo">
-                <legend>Jouw review</legend>
-                <label htmlFor="howDidYou">Hoe heb je dit recept gevonden?</label><br/>
-                <label htmlFor="options">
-                    <select name="options" id="options">
-                        <option value="nothing">Anders</option>
-                        <option value="google">Google</option>
-                        <option value="book">Kookboek</option>
-                        <option value="friend">via via</option>
-                        <option value="cantRemember">Kan ik mij niet herrineren</option>
-                    </select><br/>
-                </label><br/>
-                <label htmlFor="feedback">Opmerkingen: </label><br/>
-                <label htmlFor="inputField">
-                    <textarea
-                        name="input"
-                        id="textBox"
-                        rows="6"
-                        placeholder="Wat vond je van het recept?"
-                        value={messageValue}
-                        onChange={(e)=> setMessageValue(e.target.value)}
-                    >
-                    </textarea>
-                    <button id="buttonSend" type="submit">Versturen</button>
-                </label><br/>
-            </fieldset>
-        </form>
-        </div>
-    </div>
-      </form>
+    const [textValue, setTextValue] = React.useState("")
+    const [optionValue, setOptionValue] = React.useState("")
 
-  );
+    function sendForm(e) {
+        console.log("Naam: " + nameValue);
+        console.log("Leeftijd: " + ageValue);
+        console.log("Referentie: " + optionValue);
+        console.log("Opmerkingen: " + textValue);
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <form
+            onSubmit={(e)=> sendForm(e)}
+            >
+            <fieldset className="gegevensContainer">
+                <legend>Gegevens</legend>
+                <label>Naam:
+                    <input
+                        type="text"
+                        name={"name"}
+                        placeholder={"Wat is je naam?"}
+                        value={nameValue}
+                        onChange={(e)=>setNameValue(e.target.value)}
+                    />
+                </label>
+                <label>Leeftijd:
+                    <input
+                        type="number"
+                        name={"age"}
+                        placeholder="Wat is je leeftijd?"
+                        value={ageValue}
+                        onChange={(e)=>setAgeValue(e.target.value)}
+                    />
+                </label>
+            </fieldset>
+            <fieldset className="reviewContainer">
+                <legend>Jouw review</legend>
+                <label>Hoe heb je dit recept gevonden?</label>
+                    <select
+                        name="reference"
+                        id="reference"
+                        value={optionValue}
+                        onChange={(e)=>setOptionValue(e.target.value)}
+                    >
+                        <option value="Google">Google</option>
+                        <option value="Vriend">Vriend</option>
+                        <option value="Advertentie">Advertentie</option>
+                        <option value="Anders">Anders</option>
+                    </select>
+                <label>Opmerkingen:</label>
+                <input
+                    type="text"
+                    name="message"
+                    placeholder={"Wat vond je van het recept?"}
+                    value={textValue}
+                    onChange={(e)=>setTextValue(e.target.value)}
+                >
+                </input>
+                <button
+                    type="submit"
+
+                >
+                    Versturen</button>
+            </fieldset>
+            </form>
+        </div>
+    );
 }
 
 export default App;
